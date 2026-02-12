68b9cf80b784f671bcd933c05266cb80b7db3cf3
wp@win.com <wp@win.com>
2026-02-12 16:21:20 +0800

    update: change JetBrains to zaoanshijie

--- script/check_release.py
+++ script/check_release.py
@@ -11,7 +11,7 @@ def main():
   classifier = common.classifier()
 
   try:
-    resp = urllib.request.urlopen(urllib.request.Request('https://api.github.com/repos/JetBrains/skia-pack/releases/tags/' + version, headers=headers)).read()
+    resp = urllib.request.urlopen(urllib.request.Request('https://api.github.com/repos/zaoanshijie/skia-pack/releases/tags/' + version, headers=headers)).read()
     artifacts = [x['name'] for x in json.loads(resp.decode('utf-8'))['assets']]
     zip = 'Skia-' + version + '-' + target + '-' + build_type + '-' + machine + classifier + '.zip'
     if zip in artifacts:
